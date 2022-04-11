aca244d85b7d560568eaa985e820d1af71bbff29
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2022-04-11 19:51:57 -0300

    webApp2

---
+++ WebApplication1/WebApplication2/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication2.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Curso { get; set; }
+        public static List<Aluno> listagem = new List<Aluno>();
+        static Aluno()
+        {
+            Aluno.listagem.Add(new Aluno { Id = 1, Name = "Edjalma", Curso = "TDS" });
+            Aluno.listagem.Add(new Aluno { Id = 2, Name = "Vinícius", Curso = "Mecatronica" });
+            Aluno.listagem.Add(new Aluno { Id = 3, Name = "Andre", Curso = "Elétrica" });
+        }
+    }
+}
