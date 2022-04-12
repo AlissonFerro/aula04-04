b89df94328c5533efdea5289ca78b91d3867a265
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2022-04-11 22:15:59 -0300

    1.1

--- WebApplication1/WebApplication2/Controllers/HomeController.cs
+++ WebApplication1/WebApplication2/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         public IActionResult Index()
         {
             return View();
@@ -14,19 +14,36 @@ namespace WebApplication2.Controllers
 
         public IActionResult Listar()
         {
-            return View("Listagem",Aluno.listagem);
+            return View("Listagem", Aluno.listagem);
         }
 
         public void Adicionar(Aluno aluno)
-        { 
-            aluno.Id = Aluno.listagem.Count +1;            
-            Aluno.listagem.Add(aluno);
-            Response.Redirect("Index");
+        {
+            if (aluno.Id == 0)
+            {
+                aluno.Id = Aluno.listagem.Count + 1;
+                Aluno.listagem.Add(aluno);
+                Response.Redirect("Index");
+            }
+            else
+            {
+                Aluno alunoAtualizado = Aluno.listagem[aluno.Id];
+                alunoAtualizado.Name = aluno.Name;
+                alunoAtualizado.Curso = aluno.Curso;
+                Aluno.listagem[aluno.Id] = alunoAtualizado;
+                Response.Redirect("Index");
+            }
         }
 
         public IActionResult Formulario()
         {
             return View();
+        }
+
+        public IActionResult FormularioEditar(int id)
+        {
+            Aluno alunoEncontrado = Aluno.listagem[id];
+            return View("Formulario", alunoEncontrado);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
